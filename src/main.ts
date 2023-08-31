4c8b30b1a39c73f875c2098e2b4424d2b8038c92
sk125354 <sk125354@163.com>
2023-08-31 15:20:42 +0800

    gengxin1ceshi1

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import App from './App.vue'
-import router from './router'
-import './samples/node-api'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import './assets/dark/css-vars.css'
-import "./style.css"
-import './styles/index.scss' 
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import App from './App.vue';
+import router from './router';
+import './samples/node-api';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import './assets/dark/css-vars.css';
+import './style.css';
+import './styles/index.scss';
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+
+import { updateHandle } from './utils/update';
+if (process.env.NODE_ENV === 'development') {
+  console.log('当前环境')
+  console.log('development')
+} else {
+  updateHandle();
+}
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 createApp(App)
   .use(pinia)
   .use(router)
   .mount('#app')
   .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+    postMessage({ payload: 'removeLoading' }, '*');
+  });
